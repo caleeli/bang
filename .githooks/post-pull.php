bc6ca8755049e997660d1f575c683923f806b4c4
David Callizaya <davidcallizaya@gmail.com>
2018-11-23 14:12:34 -0400

    Print post pull actions response

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -1,8 +1,8 @@
 #!/usr/bin/env php
 <?php
 echo "composer update\n";
-shell_exec('composer update 2>&1');
+passthru('composer update 2>&1');
 echo "npm install\n";
-shell_exec('npm install 2>&1');
+passthru('npm install 2>&1');
 echo "npm run prod\n";
-shell_exec('npm run prod 2>&1');
+passthru('npm run prod 2>&1');
