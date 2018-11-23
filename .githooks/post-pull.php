d644d22d8cb96c66f969d25192b83736c9cd9127
David Callizaya <davidcallizaya@gmail.com>
2018-11-23 17:10:49 -0400

    Install and build front end

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -1,5 +1,4 @@
 <?php
 //Run in parallel composer and npm
 //run('composer update');
-//run('npm install; npm run prod');
-run('nvm install 10; nvm use 10');
+run('nvm install 10; nvm use 10; npm install; npm run prod');
