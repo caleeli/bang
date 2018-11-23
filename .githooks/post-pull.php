87fa20c26d84025c5137c4c4b813c78ab27a1c31
David Callizaya <davidcallizaya@gmail.com>
2018-11-23 17:05:40 -0400

    test nvm

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -1,4 +1,5 @@
 <?php
 //Run in parallel composer and npm
-run('composer update');
-run('npm install; npm run prod');
+//run('composer update');
+//run('npm install; npm run prod');
+run('nvm --version');
