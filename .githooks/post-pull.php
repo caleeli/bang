9431a26655af1bb4b766569f32e80f320361b21f
David Callizaya <davidcallizaya@gmail.com>
2018-11-23 14:52:14 -0400

    Fix

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -1,8 +1,5 @@
 #!/usr/bin/env php
 <?php
-echo "composer update\n";
-passthru('composer update 2>&1');
-echo "npm install\n";
-passthru('npm install 2>&1');
-echo "npm run prod\n";
-passthru('npm run prod 2>&1');
+run('composer update');
+run('npm install');
+run('npm run prod');
