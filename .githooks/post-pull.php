5a92cfa602e21fbf0ec7fb7374e7d14621dc5c3e
David Callizaya <davidcallizaya@gmail.com>
2018-11-23 17:18:24 -0400

    Rebuild node sass

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -1,4 +1,4 @@
 <?php
 //Run in parallel composer and npm
 //run('composer update');
-run('nvm install 10; npm install --global gulp-cli; npm install; npm run prod');
+run('nvm install 10; npm rebuild node-sass; npm install; npm run prod');
