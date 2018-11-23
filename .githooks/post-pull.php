dccc1a458ca47169a56048d5a56100e06b97bbd6
David Callizaya <davidcallizaya@gmail.com>
2018-11-23 17:14:25 -0400

    Install gulp

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -1,4 +1,4 @@
 <?php
 //Run in parallel composer and npm
 //run('composer update');
-run('nvm install 10; nvm use 10; npm install; npm run prod');
+run('nvm install 10; npm install --global gulp-cli; npm install; npm run prod');
